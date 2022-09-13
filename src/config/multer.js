15d282d9c882b0f04bf2db2cbce1f4d6272ebc95
jonatasJS <soaresjonatas398@gmail.com>
2022-09-13 18:10:33 -0300

    asdasdasd

--- src/config/multer.js
+++ src/config/multer.js
@@ -20,7 +20,11 @@ const storageTypes = {
     }
   }),
   s3: multerS3({
-    s3: new aws.S3(),
+    s3: new aws.S3({
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      region: process.env.AWS_DEFAULT_REGION
+    }),
     bucket: process.env.BUCKET_NAME,
     contentType: multerS3.AUTO_CONTENT_TYPE,
     acl: "public-read",
