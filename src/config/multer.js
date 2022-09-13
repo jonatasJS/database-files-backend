d947b01cf138d5bf13d37e475f6ec31479e567a5
jonatasJS <soaresjonatas398@gmail.com>
2022-09-13 13:07:40 -0300

    ASDASDASD

--- src/config/multer.js
+++ src/config/multer.js
@@ -2,7 +2,7 @@ const multer = require("multer");
 const path = require("path");
 const crypto = require("crypto");
 const aws = require("aws-sdk");
-// const multerS3 = require("multer-s3");
+const multerS3 = require("multer-s3");
 
 const storageTypes = {
   local: multer.diskStorage({
@@ -19,26 +19,26 @@ const storageTypes = {
       });
     }
   }),
-  // s3: multerS3({
-  //   s3: new aws.S3(),
-  //   bucket: process.env.BUCKET_NAME,
-  //   contentType: multerS3.AUTO_CONTENT_TYPE,
-  //   acl: "public-read",
-  //   key: (req, file, cb) => {
-  //     crypto.randomBytes(16, (err, hash) => {
-  //       if (err) cb(err);
+  s3: multerS3({
+    s3: new aws.S3(),
+    bucket: process.env.BUCKET_NAME,
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    acl: "public-read",
+    key: (req, file, cb) => {
+      crypto.randomBytes(16, (err, hash) => {
+        if (err) cb(err);
 
-  //       const fileName = `${hash.toString("hex")}-${file.originalname}`;
+        const fileName = `${hash.toString("hex")}-${file.originalname}`;
 
-  //       cb(null, fileName);
-  //     });
-  //   }
-  // })
+        cb(null, fileName);
+      });
+    }
+  })
 };
 
 module.exports = {
   dest: path.resolve(__dirname, "..", "..", "tmp", "uploads"),
-  storage: storageTypes.local, //[process.env.STORAGE_TYPE],
+  storage: storageTypes.s3, //[process.env.STORAGE_TYPE],
   limits: {
     fileSize: 2 * 1024 * 1024
   },
