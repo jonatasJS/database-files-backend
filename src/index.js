628c5edc032725b92566a75927aa88bbd035a0ef
jonatasJS <soaresjonatas398@gmail.com>
2022-09-13 18:50:24 -0300

    asdasdasd

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 app.use(
   "/files",
-  express.static(path.resolve(__dirname, "..", "public", "tmp", "uploads"))
+  express.static(path.resolve(__dirname, "..", "tmp", "uploads"))
 );
 
 app.use(require("./routes"));
