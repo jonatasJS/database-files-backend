88f758715058dc237a7642258f19fed137a8aa85
jonatasJS <soaresjonatas398@gmail.com>
2022-09-15 13:44:04 -0300

    \zx\zx\zx

--- src/index.js
+++ src/index.js
@@ -19,6 +19,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+app.use(express.static(path.resolve(__dirname, "..", "public")));
 app.use(
   "/files",
   express.static(path.resolve(__dirname, "..", "tmp", "uploads"))
